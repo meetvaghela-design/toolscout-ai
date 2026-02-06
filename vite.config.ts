3fb9df9db307bbde17c88a01af5592df7707df2c
meetvaghela-design <meetvaghela0027@gmail.com>
2026-02-06 16:36:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- Ye sabse important line hai, isse CSS load hogi
 })
